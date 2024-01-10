3837d78a35b840f09238151ebb5a5b9b8a16255e
JaneYih <840784279@qq.com>
2024-01-10 16:14:27 +0800

    关闭VS的windeployqt功能，增加加载Qt_exe相关依赖.bat批处理文件
    不知道如何设置VS的windeployqt能够附带参数-qmldir，所以取消VS的windeployqt功能，改为批处理bat文件来添加exe的相关依赖

---
+++ src/TetrisGame/加载Qt_exe相关依赖.bat
@@ -0,0 +1,23 @@
+@echo off
+
+@cd /d d:
+@cd /d D:\Qt\Qt5.13.2\5.13.2\msvc2017\bin
+
+set /p SourcePath=请输入【EXE路径】：
+set SourcePath="%SourcePath%\
+
+set /p ExeName=请输入【EXE名称】：
+set ExeName=%ExeName%"
+
+echo=
+
+set cmdString=windeployqt.exe %SourcePath%%ExeName% -qmldir "D:\Qt\Qt5.13.2\5.13.2\msvc2017\qml" --release
+echo 命令如下：
+echo %cmdString%
+echo=
+echo=
+echo 开始执行...
+%cmdString%
+
+pause
+#exit
